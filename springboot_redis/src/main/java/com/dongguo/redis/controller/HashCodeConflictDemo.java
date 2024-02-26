263ae8837db0ce5596483ad824b63d1894918d19
dongguo4812 <291320608@qq.com>
2024-02-26 14:49:20 +0800

    hash冲突

---
+++ springboot_redis/src/main/java/com/dongguo/redis/controller/HashCodeConflictDemo.java
@@ -0,0 +1,27 @@
+package com.dongguo.redis.controller;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @Author: Administrator
+ * @Date: 2024-02-26
+ * hash冲突demo
+ */
+public class HashCodeConflictDemo {
+    public static void main(String[] args) {
+        System.out.println("Aa".hashCode());//2112
+        System.out.println("BB".hashCode());
+        System.out.println("柳柴".hashCode());//851553
+        System.out.println("柴柕".hashCode());
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < 200000; i++) {
+            int hashCode = new Object().hashCode();
+            if (set.contains(hashCode)){
+                System.out.println("出现了重复的hashcode: "+hashCode+"\t 运行到"+i);//出现了重复的hashcode: 2134400190	 运行到105488
+                break;
+            }
+            set.add(hashCode);
+        }
+    }
+}
