9616835c909e6a893765d19c9f1e9562c5b3ef6e
dongguo4812 <291320608@qq.com>
2024-02-26 17:37:24 +0800

    redis布隆过滤器

---
+++ springboot_redis/src/main/java/com/dongguo/redis/controller/RedisBloomFilterDemo.java
@@ -0,0 +1,16 @@
+package com.dongguo.redis.controller;
+
+/**
+ * @Author: Administrator
+ * @Date: 2024-02-26
+ * redis布隆过滤器demo
+ */
+public class RedisBloomFilterDemo {
+
+    private static final int COUNT = 10000 * 100;
+    //误判率  大于0小于1.0
+    private static final double FPP = 0.03;
+
+
+
+}
