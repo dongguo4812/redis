ed48bc07a38e4ab2a2796496c7aacc21290aabee
dongguo4812 <291320608@qq.com>
2024-02-27 14:45:03 +0800

    Redis实现分布式锁

---
+++ redis_lock2/src/main/java/com/dongguo/lock/util/CacheConstants.java
@@ -0,0 +1,9 @@
+package com.dongguo.lock.util;
+
+/**
+ * @Author: Administrator
+ * @Date: 2024-02-27
+ */
+public class CacheConstants {
+    public  static final String CACHE_KEY_ORDER_STOCK="redis:order:stock:";
+}
---
+++ redis_lock2/src/test/java/com/dongguo/lock/RedisLock2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongguo.lock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisLock2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
