2d8b7cb9bc7a0d1abeea6af089d430a6cff590a8
dongguo <291320608@qq.com>
2024-03-08 07:40:11 +0800

    结构调整

--- redis_client/src/main/java/com/dongguo/redis/RedisClientApplication.java
+++ redis_client/src/main/java/com/dongguo/redis/RedisClientApplication.java
@@ -8,6 +8,6 @@ public class RedisClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RedisClientApplication.class, args);
+        System.out.println("http://127.0.0.1:8080/swagger-ui/index.html");
     }
-
 }
