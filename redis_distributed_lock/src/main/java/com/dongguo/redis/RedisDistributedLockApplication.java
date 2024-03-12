8a5c45f5f0dff91c03e47e7afd2521ee2785b5a0
dongguo4812 <291320608@qq.com>
2024-03-12 14:03:36 +0800

    分布式锁

--- redis_distributed_lock/src/main/java/com/dongguo/redis/RedisDistributedLockApplication.java
+++ redis_distributed_lock/src/main/java/com/dongguo/redis/RedisDistributedLockApplication.java
@@ -8,6 +8,7 @@ public class RedisDistributedLockApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RedisDistributedLockApplication.class, args);
+		System.out.println("http://127.0.0.1:8081/swagger-ui/index.html");
 	}
 
 }
