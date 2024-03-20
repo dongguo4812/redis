99076e63b33dba0e25672ebb03717a33af8f5ba6
dongguo <291320608@qq.com>
2024-03-20 15:29:00 +0800

    解决状态登录刷新问题

--- redis_dianping/src/main/java/com/dongguo/redis/config/MvcConfig.java
+++ redis_dianping/src/main/java/com/dongguo/redis/config/MvcConfig.java
@@ -1,6 +1,7 @@
 package com.dongguo.redis.config;
 
 import com.dongguo.redis.support.interceptor.LoginInterceptor;
+import com.dongguo.redis.support.interceptor.RefreshTokenInterceptor;
 import jakarta.annotation.Resource;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
@@ -12,10 +13,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class MvcConfig implements WebMvcConfigurer {
     @Resource
     private StringRedisTemplate stringRedisTemplate;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        // token刷新的拦截器
+        registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate))
+                .addPathPatterns("/**")
+                .order(0);
+
         //登录拦截器
-        registry.addInterceptor(new LoginInterceptor(stringRedisTemplate))
+        registry.addInterceptor(new LoginInterceptor())
                 //排除不需要拦截的路径，视情况添加
                 .excludePathPatterns(
                         "/v3/**",  //swagger也需要排除
@@ -28,7 +35,5 @@ public class MvcConfig implements WebMvcConfigurer {
                         "/user/sendCode",  //发送短信验证码
                         "/user/login"  //登录
                 ).order(1);
-        // token刷新的拦截器
-//        registry.addInterceptor(new RefreshTokenInterceptor(stringRedisTemplate)).addPathPatterns("/**").order(0);
     }
 }
--- redis_dianping/src/main/java/com/dongguo/redis/support/interceptor/LoginInterceptor.java
+++ redis_dianping/src/main/java/com/dongguo/redis/support/interceptor/LoginInterceptor.java
@@ -1,81 +1,22 @@
 package com.dongguo.redis.support.interceptor;
 
-import cn.hutool.core.bean.BeanUtil;
-import cn.hutool.core.util.ObjectUtil;
-import com.dongguo.redis.entity.DTO.UserDTO;
 import com.dongguo.redis.support.threadlocal.UserThreadLocalCache;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.servlet.HandlerInterceptor;
-
-import java.util.Map;
-
-import static com.dongguo.redis.utils.RedisConstants.LOGIN_USER_KEY;
 
 
 /**
  * 登录拦截器
  */
 public class LoginInterceptor implements HandlerInterceptor {
-    private StringRedisTemplate stringRedisTemplate;
 
-    public LoginInterceptor(StringRedisTemplate stringRedisTemplate) {
-        this.stringRedisTemplate = stringRedisTemplate;
-    }
-    /**
-     * 在控制器方法（即处理请求的@RequestMapping注解的方法）之前调用
-     * @param request
-     * @param response
-     * @param handler
-     * @return
-     */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-   /*     //获取session
-        HttpSession session = request.getSession();
-        //获取session中用户信息
-        Object user = session.getAttribute("user");
-        if (user == null){
-            //不存在，即未进行登录，进行拦截
+        if (UserThreadLocalCache.getUser() == null){
+            response.setStatus(401);
             return false;
         }
-        //存在，保存用户信息到threadLocal
-        UserThreadLocalCache.setUser((UserDTO)user);
-        */
-
-        String token = request.getHeader("authorization");
-        if (token == null){
-            //不存在，即未进行登录，进行拦截
-            return false;
-        }
-        Map<Object, Object> userMap = stringRedisTemplate.opsForHash().entries(LOGIN_USER_KEY + token);
-        if (ObjectUtil.isEmpty(userMap)){
-            return false;
-        }
-        UserDTO userDTO = BeanUtil.mapToBean(userMap, UserDTO.class,true);
-        if (userDTO == null){
-            //不存在，即未进行登录，进行拦截
-            return false;
-        }
-        //存在，保存用户信息到threadLocal
-        UserThreadLocalCache.setUser(userDTO);
-        //放行
         return true;
-    }
-
-    /**
-     * 在整个请求处理完成之后调用
-     * @param request
-     * @param response
-     * @param handler
-     * @param ex
-     * @throws Exception
-     */
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        // 移除ThreadLocal用户信息
-        UserThreadLocalCache.removeUser();
     }
 }
