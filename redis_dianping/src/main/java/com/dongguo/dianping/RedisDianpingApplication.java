1e598dd1fd85f5287514d3984084bff28dce43f5
dongguo <291320608@qq.com>
2024-03-22 14:06:20 +0800

    解决报名不一致暴露出无法读取core组件问题

--- redis_dianping/src/main/java/com/dongguo/dianping/RedisDianpingApplication.java
+++ redis_dianping/src/main/java/com/dongguo/dianping/RedisDianpingApplication.java
@@ -4,7 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.dongguo.dianping","com.dongguo.redis"})//com.dongguo.redis"为core包目录
 @MapperScan("com.dongguo.dianping.mapper")
 public class RedisDianpingApplication {
 
