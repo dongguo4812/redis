cf9446388f83f10f9d8311d3a9bab18b2c4a5f15
dongguo <291320608@qq.com>
2024-03-25 14:34:29 +0800

    redis_item

---
+++ redis_item/src/main/java/com/dongguo/redis/RedisItemApplication.java
@@ -0,0 +1,13 @@
+package com.dongguo.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisItemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisItemApplication.class, args);
+    }
+
+}
